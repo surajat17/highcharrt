55cdedf37a38cbd35883053cb39fe0ec876f89f1
suraj kumar <suraj.kumar@knorex.com>
2022-12-16 16:27:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import Doit from './components/Doit';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    <Doit/>
+  
+   
+    </div>
+  );
+}
+
+export default App;
